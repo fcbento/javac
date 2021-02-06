3c248187b0afb0645762deb66e51ddcbcc427035
Felie Bento <felipe.16costa@gmail.com>
2021-02-06 14:07:51 -0300

    aula

--- src/main/java/com/nelioalves/cursomc/CustomApplication.java
+++ src/main/java/com/nelioalves/cursomc/CustomApplication.java
@@ -1,8 +1,12 @@
 package com.nelioalves.cursomc;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Cidade;
+import com.nelioalves.cursomc.domain.Estado;
 import com.nelioalves.cursomc.domain.Produto;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.repositories.CidadeRepository;
+import com.nelioalves.cursomc.repositories.EstadoRepository;
 import com.nelioalves.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -20,6 +24,12 @@ public class CustomApplication implements CommandLineRunner {
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(CustomApplication.class, args);
     }
@@ -34,6 +44,18 @@ public class CustomApplication implements CommandLineRunner {
         Produto p2 = new Produto(null, "Impressora", 800.00);
         Produto p3 = new Produto(null, "Mouse", 80.00);
 
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+
+        Cidade c1 = new Cidade(null, "Uberlandia", est1);
+        Cidade c2 = new Cidade(null, "Campinas", est2);
+        Cidade c3 = new Cidade(null, "São Paulo", est2);
+
+        //Estados
+        est1.getCidades().addAll(Arrays.asList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+        //Categorias
         cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
         cat2.getProdutos().addAll(Arrays.asList(p2));
 
@@ -43,5 +65,7 @@ public class CustomApplication implements CommandLineRunner {
 
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
         produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
     }
 }

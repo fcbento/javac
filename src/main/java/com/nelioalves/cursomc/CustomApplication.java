0239526f5eaa45c5a3545f119df68b6aee82cddf
Felie Bento <felipe.16costa@gmail.com>
2021-02-06 10:16:34 -0300

    first commit

---
+++ src/main/java/com/nelioalves/cursomc/CustomApplication.java
@@ -0,0 +1,12 @@
+package com.nelioalves.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomApplication.class, args);
+    }
+}

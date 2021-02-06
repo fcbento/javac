0b054f2f62e4b6d66fc10d9de5e176daee585b8a
Felie Bento <felipe.16costa@gmail.com>
2021-02-06 11:49:39 -0300

    aula

--- src/main/java/com/nelioalves/cursomc/CustomApplication.java
+++ src/main/java/com/nelioalves/cursomc/CustomApplication.java
@@ -1,12 +1,31 @@
 package com.nelioalves.cursomc;
 
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CustomApplication {
+public class CustomApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository repo;
 
     public static void main(String[] args) {
         SpringApplication.run(CustomApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        repo.saveAll(Arrays.asList(cat1, cat2));
+
     }
 }

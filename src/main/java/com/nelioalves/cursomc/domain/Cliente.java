2c805253cf78f90a9e75a84148450ab2ef5bc72a
Felie Bento <felipe.16costa@gmail.com>
2021-02-06 22:23:22 -0300

    aula

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -1,5 +1,6 @@
 package com.nelioalves.cursomc.domain;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.nelioalves.cursomc.domain.enums.TipoCliente;
 
 import javax.persistence.*;
@@ -20,6 +21,7 @@ public class Cliente implements Serializable {
     private String cpfCnpj;
     private Integer tipo;
 
+    @JsonManagedReference
     @OneToMany(mappedBy = "cliente")
     private List<Endereco> enderecos = new ArrayList<>();
 

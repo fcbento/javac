c177996d6942eccfaeb7f0467b67d480c79b390d
Felie Bento <felipe.16costa@gmail.com>
2021-02-27 00:24:02 -0300

    aula

--- src/main/java/com/nelioalves/cursomc/services/MockEmailServic.java
+++ src/main/java/com/nelioalves/cursomc/services/MockEmailServic.java
@@ -4,6 +4,8 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 
 public class MockEmailServic extends AbstractEmailService{
 
@@ -12,6 +14,13 @@ public class MockEmailServic extends AbstractEmailService{
     @Override
     public void sendEmail(SimpleMailMessage msg) {
         LOG.info("Simulando envio de email");
+        LOG.info(msg.toString());
+        LOG.info("Email enviado");
+    }
+
+    @Override
+    public void sendHtmlEmail(MimeMessage msg) {
+        LOG.info("Simulando envio de email html");
         LOG.info(msg.toString());
         LOG.info("Email enviado");
     }
